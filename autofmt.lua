464db3791b09a4b66f5cd8eec8ef549806a422ff
Quinn Edenfield <quinnyxboy@gmail.com>
2024-10-14 15:30:39 -0700

    Modified:   autofmt.lua

--- autofmt.lua
+++ autofmt.lua
@@ -13,6 +13,7 @@ fmtCommands["csharp"] = "clang-format -i"
 fmtCommands["racket"] = "raco fmt --width 80 --max-blank-lines 2 -i"
 fmtCommands["javascript"] = "prettier --write --loglevel silent"
 fmtCommands["rust"] = "rustfmt +nightly"
+fmtCommands["nix"] = "alejandra -qq"
 
 config.RegisterCommonOption("autofmt", "fmt-onsave", true)
 
